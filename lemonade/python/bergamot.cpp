5146d300d6a5300323173b105dea56c615af0776
Jerin Philip <jphilip@ed.ac.uk>
2021-10-29 12:47:12 +0000

    Pybind11 now compiles, again

--- lemonade/python/bergamot.cpp
+++ lemonade/python/bergamot.cpp
@@ -5,6 +5,7 @@
 #include <translator/response.h>
 #include <translator/response_options.h>
 #include <translator/service.h>
+#include <translator/translation_model.h>
 
 #include <iostream>
 #include <string>
@@ -17,10 +18,11 @@ using marian::bergamot::AnnotatedText;
 using marian::bergamot::ByteRange;
 using marian::bergamot::ConcatStrategy;
 using marian::bergamot::Point;
-using marian::bergamot::QualityScoreType;
 using marian::bergamot::Response;
 using marian::bergamot::ResponseOptions;
-using marian::bergamot::Service;
+using Service = marian::bergamot::AsyncService;
+using _Model = marian::bergamot::TranslationModel;
+using Model = std::shared_ptr<_Model>;
 
 PYBIND11_MAKE_OPAQUE(std::vector<Response>);
 PYBIND11_MAKE_OPAQUE(std::vector<std::string>);
@@ -31,38 +33,46 @@ PYBIND11_MAKE_OPAQUE(std::vector<Alignment>);
 // Jupyter notebook, @jerinphilip is not doing efficiency here.
 
 class ServicePyAdapter {
- public:
-  ServicePyAdapter(const std::string &config) {
-    py::scoped_ostream_redirect outstream(std::cout,                                 // std::ostream&
-                                          py::module_::import("sys").attr("stdout")  // Python output
+public:
+  ServicePyAdapter(const Service::Config &config) {
+    py::scoped_ostream_redirect outstream(
+        std::cout,                                // std::ostream&
+        py::module_::import("sys").attr("stdout") // Python output
     );
-    py::scoped_ostream_redirect errstream(std::cerr,                                 // std::ostream&
-                                          py::module_::import("sys").attr("stderr")  // Python output
+    py::scoped_ostream_redirect errstream(
+        std::cerr,                                // std::ostream&
+        py::module_::import("sys").attr("stderr") // Python output
     );
 
     py::call_guard<py::gil_scoped_release> gil_guard();
     service_.reset(std::move(new Service(config)));
   }
-  Response translate(std::string input, const ResponseOptions options) {
-    py::scoped_ostream_redirect outstream(std::cout,                                 // std::ostream&
-                                          py::module_::import("sys").attr("stdout")  // Python output
+
+  Response translate(Model model, std::string input,
+                     const ResponseOptions &options) {
+    py::scoped_ostream_redirect outstream(
+        std::cout,                                // std::ostream&
+        py::module_::import("sys").attr("stdout") // Python output
     );
-    py::scoped_ostream_redirect errstream(std::cerr,                                 // std::ostream&
-                                          py::module_::import("sys").attr("stderr")  // Python output
+    py::scoped_ostream_redirect errstream(
+        std::cerr,                                // std::ostream&
+        py::module_::import("sys").attr("stderr") // Python output
     );
     py::call_guard<py::gil_scoped_release> gil_guard();
 
     std::promise<Response> responsePromise;
     std::future<Response> responseFuture = responsePromise.get_future();
 
-    auto callback = [&responsePromise](Response &&response) { responsePromise.set_value(std::move(response)); };
+    auto callback = [&responsePromise](Response &&response) {
+      responsePromise.set_value(std::move(response));
+    };
 
-    service_->translate(std::move(input), std::move(callback), options);
+    service_->translate(model, std::move(input), std::move(callback), options);
     responseFuture.wait();
     return responseFuture.get();
   }
 
- private:
+private:
   std::unique_ptr<Service> service_{nullptr};
 };
 
@@ -72,30 +82,29 @@ PYBIND11_MODULE(pybergamot, m) {
       .def_readonly("begin", &ByteRange::begin)
       .def_readonly("end", &ByteRange::end)
       .def("__repr__", [](const ByteRange &range) {
-        return "{" + std::to_string(range.begin) + ", " + std::to_string(range.end) + "}";
+        return "{" + std::to_string(range.begin) + ", " +
+               std::to_string(range.end) + "}";
       });
 
   py::class_<AnnotatedText>(m, "AnnotatedText")
       .def(py::init<>())
       .def("numWords", &AnnotatedText::numWords)
       .def("numSentences", &AnnotatedText::numSentences)
-      .def("isUnknown", &AnnotatedText::isUnknown)
       .def("word",
-           [](const AnnotatedText &annotatedText, size_t sentenceIdx, size_t wordIdx) -> std::string {
+           [](const AnnotatedText &annotatedText, size_t sentenceIdx,
+              size_t wordIdx) -> std::string {
              auto view = annotatedText.word(sentenceIdx, wordIdx);
              return std::string(view.data(), view.size());
            })
       .def("sentence",
-           [](const AnnotatedText &annotatedText, size_t sentenceIdx) -> std::string {
+           [](const AnnotatedText &annotatedText,
+              size_t sentenceIdx) -> std::string {
              auto view = annotatedText.sentence(sentenceIdx);
              return std::string(view.data(), view.size());
            })
       .def("wordAsByteRange", &AnnotatedText::wordAsByteRange)
       .def("sentenceAsByteRange", &AnnotatedText::sentenceAsByteRange)
       .def_readonly("text", &AnnotatedText::text);
-
-  py::bind_vector<std::vector<Point>>(m, "Alignment");
-  py::bind_vector<std::vector<Alignment>>(m, "VectorAlignment");
 
   py::class_<Response>(m, "Response")
       .def(py::init<>())
@@ -110,30 +119,21 @@ PYBIND11_MODULE(pybergamot, m) {
       .def_readwrite("qualityScores", &ResponseOptions::qualityScores)
       .def_readwrite("alignment", &ResponseOptions::alignment)
       .def_readwrite("sentenceMappings", &ResponseOptions::sentenceMappings)
-      .def_readwrite("alignmentThreshold", &ResponseOptions::alignmentThreshold)
-      .def_readwrite("qualityScoreType", &ResponseOptions::alignmentThreshold)
-      .def_readwrite("concatStrategy", &ResponseOptions::alignmentThreshold);
+      .def_readwrite("alignmentThreshold",
+                     &ResponseOptions::alignmentThreshold);
 
   py::enum_<ConcatStrategy>(m, "ConcatStrategy")
       .value("FAITHFUL", ConcatStrategy::FAITHFUL)
       .value("SPACE", ConcatStrategy::SPACE)
       .export_values();
 
-  py::enum_<QualityScoreType>(m, "QualityScoreType")
-      .value("FREE", QualityScoreType::FREE)
-      .value("EXPENSIVE", QualityScoreType::EXPENSIVE)
-      .export_values();
-
   py::bind_vector<std::vector<std::string>>(m, "VectorString");
   py::class_<ServicePyAdapter>(m, "Service")
-      .def(py::init<const std::string &>())
+      .def(py::init<const Service::Config &>())
       .def("translate", &ServicePyAdapter::translate);
 
-  py::class_<Point>(m, "Point")
-      .def_readonly("src", &Point::src)
-      .def_readonly("tgt", &Point::tgt)
-      .def_readonly("prob", &Point::prob)
-      .def("__repr__", [](const Point &p) {
-        return "{(" + std::to_string(p.src) + ", " + std::to_string(p.tgt) + ") = " + std::to_string(p.prob) + "}";
-      });
+  py::class_<Service::Config>(m, "ServiceConfig")
+      .def_readwrite("numWorkers", &Service::Config::numWorkers);
+
+  py::class_<_Model, std::shared_ptr<_Model>>(m, "Model");
 }
