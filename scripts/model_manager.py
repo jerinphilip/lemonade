36cfe2293208c2271db68e240fd8a5be18a4982c
Jerin Philip <jphilip@ed.ac.uk>
2021-11-26 15:19:57 +0000

    Add alignment to fetch to not cause aborts, this is bad but temporary ok

--- scripts/model_manager.py
+++ scripts/model_manager.py
@@ -78,6 +78,7 @@ def patch_marian_for_bergamot(fpath, output_path, quality=False):
         'max-length-break': 128,
         'mini-batch-words': 1024,
         'workspace': 128, # shipped models use big workspaces. We'd prefer to keep it low.
+        'alignment': 'soft',
     })
 
     if quality:
