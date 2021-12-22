013cf125385cf0f2e216ddeabcc295853bbdd011
Jerin Philip <jerin.philip@research.iiit.ac.in>
2021-12-22 12:57:37 +0000

    Using inventory loads instead of full file path

--- bindings/python/cli.py
+++ bindings/python/cli.py
@@ -5,11 +5,12 @@ from collections import Counter
 from collections import defaultdict
 from argparse import ArgumentParser
 from ._bergamot import Service, ResponseOptions, ServiceConfig, VectorString
+from .config import Config, get_inventory, hardCodeFpaths
 
 
 if __name__ == '__main__':
     parser = ArgumentParser("Translate a sample blob of text in python")
-    parser.add_argument('--model-config', type=str, help="Path to model file to use in tag-transfer translation", required=True)
+    parser.add_argument('--model-code', type=str, help="Path to model file to use in tag-transfer translation", required=True)
     parser.add_argument('--num-workers', type=int, help="Number of worker threads to use to translate", default=4)
     parser.add_argument('--cache-size', type=int, help="How many sentences to hold in cache", default=2000)
     parser.add_argument('--cache-mutex-buckets', type=int, help="How many mutex buckets to use to reduce contention in cache among workers", default=20)
@@ -26,21 +27,24 @@ if __name__ == '__main__':
     service = Service(config)
 
     # Work with one model, loaded from config file
-    model = service.modelFromConfigPath(args.model_config)
+    appConfig = Config()
+    inventory = get_inventory(appConfig.url, appConfig.models_file)
+    model_config =  { entry['code']: entry for entry in inventory["models"] }
+    model_entry = model_config[args.model_code]
+    fprefix = hardCodeFpaths(model_entry["url"])
+    model_dir = os.path.join(appConfig.models_dir, fprefix)
+    model_config = os.path.join(model_dir, 'config.bergamot.yml')
+
+    model = service.modelFromConfigPath(model_config)
 
     # Configure a few options which require how a Response is constructed
     options = ResponseOptions();
     options.alignment = True
     options.qualityScores = True
 
-    inputs = [
-        "Hello World!",
-        "Goodbye World!"
-    ]
-
-    responses = service.translate(model, VectorString(inputs), options)
+    source = sys.stdin.read()
+    responses = service.translate(model, VectorString([source]), options)
 
     for response in responses:
-        print('[src] > ', response.source.text)
-        print('[tgt] > ', response.target.text)
+        print(response.target.text)
 
--- bindings/python/pkgmgr.py
+++ bindings/python/pkgmgr.py
@@ -2,53 +2,10 @@
 import requests
 from argparse import ArgumentParser
 import typing
-import json
 import yaml
-from dataclasses import dataclass
 import os
 import tarfile
-from urllib.parse import urlparse
-from appdirs import AppDirs
-
-APP = "lemonade"
-appDir = AppDirs("lemonade")
-
-@dataclass
-class Config:
-    url : str = "https://translatelocally.com/models.json"
-    cache_dir: str = appDir.user_cache_dir
-    config_dir: str = appDir.user_config_dir
-    models_file: str = os.path.join(appDir.user_config_dir, "models.json")
-    data_dir: str = appDir.user_data_dir
-    archive_dir: str = os.path.join(appDir.user_data_dir, "archives")
-    models_dir: str = os.path.join(appDir.user_data_dir, "models")
-
-
-def create_required_dirs(config):
-    os.makedirs(config.cache_dir, exist_ok=True)
-    os.makedirs(config.config_dir, exist_ok=True)
-    os.makedirs(config.data_dir, exist_ok=True)
-    os.makedirs(config.models_dir, exist_ok=True)
-    os.makedirs(config.archive_dir, exist_ok=True)
-
-def get_inventory(url, save_location, force_download=False):
-    def get_inventory_fresh(url):
-        response = requests.get(url)
-        return response.text
-
-    def write_inventory_to_file(inventory, save_location):
-        with open(save_location, "w+") as models_file:
-            models_file.write(inventory)
-
-    if force_download or not os.path.exists(save_location):
-        inventory = get_inventory_fresh(url)
-        write_inventory_to_file(inventory, save_location)
-        return json.loads(inventory)
-    else:
-        with open(save_location) as models_file:
-            data = json.load(models_file)
-            return data
-
+from .config import Config, create_required_dirs, get_inventory, hardCodeFpaths, listModels
 
 def download_archive(url, save_location, force_download=False):
     if force_download or not os.path.exists(save_location):
@@ -58,13 +15,6 @@ def download_archive(url, save_location, force_download=False):
         with open(save_location, 'wb') as handle:
             for block in response.iter_content(1024):
                 handle.write(block)
-
-
-def hardCodeFpaths(url):
-    o = urlparse(url)
-    fname = os.path.basename(o.path) # something tar.gz.
-    fname_without_extension = fname.replace(".tar.gz", "")
-    return fname_without_extension
 
 
 def patch_marian_for_bergamot(fpath, output_path, quality=False):
@@ -114,18 +64,6 @@ def download(config):
             bergamot_config_path = os.path.join(link, "config.bergamot.yml")
             patch_marian_for_bergamot(config_path, bergamot_config_path)
             print("Done.")
-
-def listModels(config):
-    data = get_inventory(config.url, config.models_file)
-    print("The following models are available:\n")
-    counter = 0
-    for model in data["models"]:
-        fprefix = hardCodeFpaths(model["url"])
-        model_dir = os.path.join(config.models_dir, fprefix)
-        if (os.path.exists(model_dir)):
-            counter += 1
-            print(' {}.'.format(str(counter).rjust(4)), model["code"], model["name"])
-    print()
 
 
 if __name__ == '__main__':
