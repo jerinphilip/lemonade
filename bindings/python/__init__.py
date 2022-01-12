f7030d032fb48b552632f8e94fa651b070ad0760
Jerin Philip <jerin.philip@research.iiit.ac.in>
2022-01-12 13:08:28 +0000

    Setting up versioning

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -1,4 +1,5 @@
 try:
     from ._bergamot import *
+    from _version import __version__
 except ImportError:
     raise
--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import os
 import re
 import subprocess
 import sys
+import io
 
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
 
 
 # Convert distutils Windows platform specifiers to CMake -A arguments
@@ -15,7 +16,6 @@ PLAT_TO_CMAKE = {
     "win-arm32": "ARM",
     "win-arm64": "ARM64",
 }
-
 
 # A CMakeExtension needs a sourcedir instead of a file list.
 # The name must be the _single_ output extension from the CMake build.
@@ -126,19 +126,68 @@ class CMakeBuild(build_ext):
         )
 
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+version = None
+with open(os.path.join(here, "lemonade.version")) as f:
+    version = f.read().strip()
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Pushing git tags…")
+        os.system("git push --tags")
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        sys.exit()
+
+
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="bergamot",
-    version="0.0.1",
+    version=version,
     author="Jerin Philip",
     author_email="jerinphilip@live.in",
-    description="Bergamot translation python binding.",
+    description="Bergamot translator python binding.",
     long_description="",
     ext_modules=[CMakeExtension("bergamot/_bergamot")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
+    license_files=("LICENSE.txt", ),
     python_requires=">=3.6",
     packages=['bergamot'],
     package_dir={'bergamot': 'bindings/python'},
