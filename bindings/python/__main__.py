c59880986d596a308a7a0971d135f068794932fa
Jerin Philip <jerinphilip@live.in>
2022-01-16 16:28:11 +0000

    Refactor commands (#41)
    Commands are now populated in a registry. Still not ideal, but better
    than before. Adds selective model download, which speeds up CI.
    
    More type-annotations as code is moved around.

--- bindings/python/__main__.py
+++ bindings/python/__main__.py
@@ -1,35 +1,11 @@
 import argparse
-import os
 import sys
 from argparse import ArgumentParser
-from collections import Counter, defaultdict
 
-from . import ResponseOptions, Service, ServiceConfig, VectorString
-from .config import repository
+from .cmds import CMDS
 
 
-def translate_fn(args):
-    # Build service
-    config = ServiceConfig(numWorkers=args.num_workers)
-    service = Service(config)
-
-    # Work with one model, loaded from config file
-    model_config = repository.modelConfigPath(args.model)
-    model = service.modelFromConfigPath(model_config)
-
-    # Configure a few options which require how a Response is constructed
-    options = ResponseOptions(
-        alignment=args.alignment, qualityScores=args.quality_scores, HTML=args.html
-    )
-
-    source = sys.stdin.read()
-    responses = service.translate(model, VectorString([source]), options)
-
-    for response in responses:
-        print(response.target.text, end="")
-
-
-def main():
+def main() -> None:
     parser = ArgumentParser("bergamot")
     subparsers = parser.add_subparsers(
         title="actions",
@@ -38,61 +14,13 @@ def main():
         dest="action",
     )
 
-    ls = subparsers.add_parser("ls")
-
-    dl = subparsers.add_parser("download")
-    dl.add_argument(
-        "-m",
-        "--model",
-        type=str,
-        required=False,
-        default=None,
-        help="Fetch model with given code. Use ls to list available models",
-    )
-
-    dl.add_argument("--all", type=bool, default=True)
-
-    translate = subparsers.add_parser("translate")
-    translate.add_argument(
-        "-m",
-        "--model",
-        type=str,
-        help="Path to model file to use in tag-transfer translation",
-        required=True,
-    )
-    translate.add_argument(
-        "--num-workers",
-        type=int,
-        help="Number of worker threads to use to translate",
-        default=4,
-    )
-
-    # Tweak response-options for quick HTML in out via commandline
-    options = translate.add_argument_group("response-options")
-    options.add_argument("--html", type=bool, default=False)
-    options.add_argument("--alignment", type=bool, default=False)
-    options.add_argument("--quality-scores", type=bool, default=False)
+    for key, cls in CMDS.items():
+        cls.embed_subparser(key, subparsers)
 
     args = parser.parse_args()
 
-    if args.action == "download":
-        for model in repository.models(filter_downloaded=False):
-            repository.download(model)
-    elif args.action == "ls":
-        print("Available models: ")
-        for counter, identifier in enumerate(
-            repository.models(filter_downloaded=True), 1
-        ):
-            model = repository.model(identifier)
-            print(
-                " {}.".format(str(counter).rjust(4)),
-                model["code"],
-                model["name"],
-            )
-        print()
-
-    elif args.action == "translate":
-        translate_fn(args)
+    if args.action in CMDS:
+        CMDS[args.action].execute(args)
     else:
         parser.print_help(sys.stderr)
         sys.exit(1)
