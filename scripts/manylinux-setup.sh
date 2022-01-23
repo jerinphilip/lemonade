e0636f9b60fda7ac6dbef692c5636ecf2daa319c
Jerin Philip <jerin.philip@research.iiit.ac.in>
2022-01-23 01:11:36 +0000

    Install python before MKL: Fast fail

--- scripts/manylinux-setup.sh
+++ scripts/manylinux-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+VERSION=$(python -c "import platform; print(platform.python_version().replace('.', '')[:-1])")
+yum install python${VERSION}-devel
+
 yum-config-manager -y --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 yum install -y intel-mkl
 
-VERSION=$(python -c "import platform; print(platform.python_version().replace('.', '')[:-1])")
-yum install python${VERSION}-devel
 
 
