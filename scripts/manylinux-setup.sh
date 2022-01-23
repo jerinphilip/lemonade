89cd32695553084312f17057e99d91e5d3ab3bc3
Jerin Philip <jerin.philip@research.iiit.ac.in>
2022-01-23 01:26:50 +0000

    Fix expansion

--- scripts/manylinux-setup.sh
+++ scripts/manylinux-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -eo pipefail
+set -eo pipefail;
+set -x;
 
 PACKAGES=$(echo ${PY_VERSIONS} | grep "[0-9]*" -o | sort | uniq | xargs -I% echo python%-devel)
-yum install ${PACKAGES}
+yum install ${PACKAGES[@]}
 
 yum-config-manager -y --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 yum install -y intel-mkl
