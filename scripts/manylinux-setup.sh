d24b5c467bc327a9546d4f42cf282a978c8fb2dd
Jerin Philip <jerin.philip@research.iiit.ac.in>
2022-01-23 01:57:07 +0000

    We should be able to do without system package?

--- scripts/manylinux-setup.sh
+++ scripts/manylinux-setup.sh
@@ -4,7 +4,6 @@ set -eo pipefail;
 set -x;
 
 yum install python*-devel -y
-yum install -y pybind11-devel
 
 yum-config-manager -y --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 yum install -y intel-mkl
