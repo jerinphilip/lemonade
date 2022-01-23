9a9e7265b6955fdc7ad8eabbd9a62b050ea24db3
Jerin Philip <jerin.philip@research.iiit.ac.in>
2022-01-23 01:02:05 +0000

    Trying to install python-devel of corresponding version

--- scripts/manylinux-setup.sh
+++ scripts/manylinux-setup.sh
@@ -3,4 +3,7 @@
 yum-config-manager -y --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
 yum install -y intel-mkl
 
+VERSION=$(python3 -c "import platform; print(platform.python_version().replace('.', '')[:-1])")
+yum install python${VERSION}-devel
+
 
