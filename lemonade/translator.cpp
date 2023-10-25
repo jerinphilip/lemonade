39abd4501530f5731221496beded14dd019a436a
Jerin Philip <jerinphilip@live.in>
2023-10-25 14:22:12 +0530

    Update to newer code

--- lemonade/translator.cpp
+++ lemonade/translator.cpp
@@ -18,16 +18,16 @@ Inventory::Inventory() {
   QCoreApplication(argc, argv); // NOLINT
   QCoreApplication::setApplicationName("bergamot");
 
-  modelsJSON_ = QStandardPaths::locate(QStandardPaths::AppConfigLocation,
-                                       "browsermt/models.json")
+  models_json_ = QStandardPaths::locate(QStandardPaths::AppConfigLocation,
+                                        "browsermt/models.json")
+                     .toStdString();
+
+  inventory_ = load(models_json_);
+
+  models_dir_ = QStandardPaths::locate(QStandardPaths::AppDataLocation,
+                                       "models/browsermt",
+                                       QStandardPaths::LocateDirectory)
                     .toStdString();
-
-  inventory_ = readInventoryFromDisk(modelsJSON_);
-
-  modelsDir_ = QStandardPaths::locate(QStandardPaths::AppDataLocation,
-                                      "models/browsermt",
-                                      QStandardPaths::LocateDirectory)
-                   .toStdString();
   // LEMONADE_ABORT_IF(!inventory_.HasMember("models"), "No models found");
   const rapidjson::Value &models = inventory_["models"];
 
@@ -59,7 +59,11 @@ std::optional<Info> Inventory::query(const std::string &source,
 
 size_t Inventory::Hash::operator()(const Direction &direction) const {
   auto hash_combine = [](size_t &seed, size_t next) {
-    seed ^= std::hash<size_t>{}(next) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    seed ^= (std::hash<size_t>{}(next) //
+             + 0x9e3779b9              // NOLINT
+             + (seed << 6)             // NOLINT
+             + (seed >> 2)             // NOLINT
+    );
   };
 
   size_t seed = std::hash<std::string>{}(direction.first);
@@ -67,97 +71,54 @@ size_t Inventory::Hash::operator()(const Direction &direction) const {
   return seed;
 }
 
-rapidjson::Document
-Inventory::readInventoryFromDisk(const std::string &modelsJSON) {
-  FILE *fp = fopen(modelsJSON.c_str(), "r"); // non-Windows use "r"
+rapidjson::Document Inventory::load(const std::string &json) {
+  FILE *fp = fopen(json.c_str(), "r"); // non-Windows use "r"
 
   if (!fp) {
-    LOG("File %s not found", modelsJSON.c_str());
+    LOG("File %s not found", json.c_str());
     std::abort();
   }
-  char readBuffer[65536];
-  rapidjson::FileReadStream fReadStream(fp, readBuffer, sizeof(readBuffer));
-  rapidjson::Document d;
-  d.ParseStream(fReadStream);
+
+  constexpr size_t kMaxBufferSize = 65536;
+  char buffer[kMaxBufferSize];
+  rapidjson::FileReadStream stream(fp, buffer, sizeof(buffer));
+  rapidjson::Document document;
+  document.ParseStream(stream);
   fclose(fp);
-  return d;
+
+  return document;
 }
 
-std::pair<std::string, std::string> Inventory::configFile(const Info &info) {
-  return std::make_pair(modelsDir_ + "/" + info.code, "config.bergamot.yml");
+std::pair<std::string, std::string> Inventory::config_file(const Info &info) {
+  return std::make_pair(models_dir_ + "/" + info.code, "config.bergamot.yml");
 }
 
-std::string Model::translate(std::string input) {
-  using namespace slimt;
-  bool add_eos = true;
-  auto [words, views] = vocabulary_.encode(input, add_eos);
-  uint64_t batch_size = 1;
-  uint64_t sequence_length = words.size();
-  Batch batch(batch_size, sequence_length, vocabulary_.pad_id());
-  batch.add(words);
-
-  auto sentences = model_.translate(batch);
-  auto sentence = sentences[0];
-  auto [result, tgt_views] = vocabulary_.decode(sentence);
-  return result;
-}
-
-Record<std::string> Model::load_path(const std::string &root,
-                                     YAML::Node &config) {
+std::shared_ptr<slimt::Model> make_model(const std::string &root,
+                                         YAML::Node &config) {
   auto prefix_browsermt = [&root](const std::string &path) {
     return root + "/" + path;
   };
 
   using Strings = std::vector<std::string>;
 
-  Strings model_paths = config["models"].as<Strings>();
+  auto model_paths = config["models"].as<Strings>();
   std::string model_path = prefix_browsermt(model_paths[0]);
   LOG("model_path: %s", model_path.c_str());
 
-  Strings vocab_paths = config["vocabs"].as<Strings>();
+  auto vocab_paths = config["vocabs"].as<Strings>();
   std::string vocab_path = prefix_browsermt(vocab_paths[0]);
 
-  Strings shortlist_args = config["shortlist"].as<Strings>();
+  auto shortlist_args = config["shortlist"].as<Strings>();
   std::string shortlist_path = prefix_browsermt(shortlist_args[0]);
 
-  Record<std::string> path{
+  slimt::Package<std::string> path{
       .model = model_path,        //
-      .vocab = vocab_path,        //
+      .vocabulary = vocab_path,   //
       .shortlist = shortlist_path //
   };
-  return path;
-}
 
-Record<slimt::io::MmapFile> Model::mmap_from(Record<std::string> &path) {
-  using namespace slimt;
-  auto prefix_browsermt = [](const std::string &path) { return path; };
-  return {
-      .model = io::MmapFile(path.model),         //
-      .vocab = io::MmapFile(path.vocab),         //
-      .shortlist = io::MmapFile(path.shortlist), //
-  };
-}
-
-slimt::Model Model::load_model(slimt::Vocabulary &vocabulary,
-                               Record<slimt::io::MmapFile> &mmap) {
-  using namespace slimt;
-  auto items = io::loadItems(mmap_.model.data());
-  ShortlistGenerator shortlist_generator(             //
-      mmap_.shortlist.data(), mmap_.shortlist.size(), //
-      vocabulary, vocabulary                          //
-  );
-
-  size_t encoder_layers = 6;
-  size_t decoder_layers = 2;
-  size_t ffn_depth = 2;
-
-  slimt::Model model(                //
-      Tag::tiny11,                   //
-      vocabulary,                    //
-      std::move(items),              //
-      std::move(shortlist_generator) //
-  );
-  return model;
+  slimt::Model::Config model_config;
+  return std::make_shared<slimt::Model>(model_config, path);
 }
 
 void Translator::set_direction(const std::string &source,
@@ -182,30 +143,36 @@ void Translator::set_direction(const std::string &source,
 }
 
 std::string Translator::translate(const std::string &source) {
+  slimt::Options options{.html = false};
+
   if (m1_ && m2_) {
     // Pivoting.
-    std::string pivot = m1_->translate(source);
-    std::string translation = m2_->translate(pivot);
-    return translation;
+    std::future<slimt::Response> future =
+        service_.pivot(m1_, m2_, source, options);
+    slimt::Response response = future.get();
+    return response.target.text;
   }
 
-  assert(m1_.get() != nullptr);
+  assert(m1_ != nullptr);
 
-  return m1_->translate(source);
+  std::future<slimt::Response> future =
+      service_.translate(m1_, source, options);
+  slimt::Response response = future.get();
+  return response.target.text;
 }
 
-std::unique_ptr<Model> Translator::get_model(const Info &info) {
-  auto [model_root, config] = inventory_.configFile(info);
+std::shared_ptr<slimt::Model> Translator::get_model(const Info &info) {
+  auto [model_root, config] = inventory_.config_file(info);
   LOG("Model model_root %s, config %s", model_root.c_str(), config.c_str());
   YAML::Node tree = YAML::LoadFile(model_root + "/" + config);
-  LOG("Model building from bundle took %f seconds.\n", -1.0f);
-  return std::make_unique<Model>(model_root, tree);
+  LOG("Model building from bundle took %f seconds.\n", -1.0F);
+  return make_model(model_root, tree);
 }
 
 void FakeTranslator::set_direction(const std::string &source,
                                    const std::string &target) {}
 
-std::string FakeTranslator::translate(std::string input) {
+std::string FakeTranslator::translate(std::string input) { // NOLINT
 
   std::string response;
   if (input.empty()) {
@@ -217,7 +184,7 @@ std::string FakeTranslator::translate(std::string input) {
   // Simulates translation in some capacity.
   auto transform = [](size_t length) -> std::string {
     std::mt19937_64 generator;
-    size_t truncate_length = 6;
+    constexpr size_t kTruncateLength = 6;
     generator.seed(length);
     std::string target;
     for (size_t i = 0; i < length; i++) {
@@ -225,10 +192,11 @@ std::string FakeTranslator::translate(std::string input) {
         target += " ";
       }
       size_t value = generator();
-      std::string hex(' ', 20);
+      constexpr size_t kMaxLength = 20;
+      std::string hex(kMaxLength, ' ');
       std::sprintf(hex.data(), "%x", static_cast<unsigned int>(value));
       // 2 to truncate 0x.
-      target += hex.substr(2, truncate_length);
+      target += hex.substr(2, kTruncateLength);
     }
     return target;
   };
@@ -236,8 +204,7 @@ std::string FakeTranslator::translate(std::string input) {
   auto token_count = [](const std::string &input) -> size_t {
     std::string token;
     size_t count = 0;
-    for (size_t i = 0; i < input.size(); i++) {
-      char c = input[i];
+    for (char c : input) {
       if (isspace(c)) {
         // Check for space.
         if (!token.empty()) {
